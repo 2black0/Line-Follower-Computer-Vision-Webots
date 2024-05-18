e87489a96467ace7e00d2de1735f9d908562ad82
2black0 <2black0@gmail.com>
2024-05-19 06:24:34 +0700

    update

--- controllers/robot_controller/robot_controller.py
+++ controllers/robot_controller/robot_controller.py
@@ -25,8 +25,8 @@ class LineFollower:
         self.MaxVelocity = 6.28
         
         self.KpFollow = 0.025
-        self.KiFollow = 0.015
-        self.KdFollow = 0.009
+        self.KiFollow = 0.0006
+        self.KdFollow = 0.006
         self.IntegralFollow = 0
         self.PreviousErrorFollow = 0
         
@@ -41,12 +41,15 @@ class LineFollower:
         
         return CameraImage
 
-    def GetReference(self, CameraImage, SensorRow, SensorWidth, draw=False):
+    def GetReference(self, CameraImage, SensorRow, SensorWidth, drawDot=False, drawBox=False):
         VerticalStart = int(self.CameraWidth * ((1-SensorWidth)/2))
         VerticalEnd = int(self.CameraWidth - self.CameraWidth * ((1-SensorWidth)/2))
         HorizontalStart = self.RowHeight * SensorRow
         HorizontalEnd = HorizontalStart + self.RowHeight
-                
+        
+        LeftTopPoint = (VerticalStart, HorizontalStart)
+        RightBottomPoint = (VerticalEnd, HorizontalEnd)
+            
         Roi = CameraImage[int(HorizontalStart):int(HorizontalEnd), int(VerticalStart):int(VerticalEnd)]
         RoiGray = cv2.cvtColor(Roi, cv2.COLOR_BGR2GRAY)
         _, RoiThreshold = cv2.threshold(RoiGray, 50, 255, cv2.THRESH_BINARY_INV)
@@ -57,54 +60,19 @@ class LineFollower:
             RoiCx = int(RoiMoments['m10'] / RoiMoments['m00'])
             RoiCy = int(RoiMoments['m01'] / RoiMoments['m00'])
             RoiDetected = True
-            CameraImage[HorizontalStart:HorizontalEnd, VerticalStart:VerticalEnd] = Roi
-            if draw == True:
-                cv2.circle(Roi, (RoiCx, RoiCy), 3, (0, 255, 0), -1)
+            if drawDot:
+                cv2.circle(Roi, (RoiCx, RoiCy), 3, (0, 0, 255), -1)
+            if drawBox:
+                cv2.rectangle(CameraImage, LeftTopPoint, RightBottomPoint, (255, 0, 0), 2)
         else:
             RoiCx = 0
             RoiCy = 0
-            
-        #print(RoiDetected, SensorRow, RoiCx, RoiCy)
-        return CameraImage, RoiDetected, RoiCx, RoiCy
+        return CameraImage, RoiDetected, RoiCx, RoiCy    
 
-    def GetReference2(self, CameraImage, Row, draw=False):
-        #VerticalStart = 0
-        #VerticalEnd = self.CameraWidth
-        #HorizontalStart = self.RowHeight * Row
-        #HorizontalEnd = HorizontalStart + self.RowHeight
-        
-        #Roi = CameraImage[int(HorizontalStart):int(HorizontalEnd), int(VerticalStart):int(VerticalEnd)]
-        RoiGray = cv2.cvtColor(CameraImage, cv2.COLOR_BGR2GRAY)
-        RoiBlurred = cv2.GaussianBlur(RoiGray, (5, 5), 0)
-        _, RoiThreshold = cv2.threshold(RoiBlurred, 50, 255, cv2.THRESH_BINARY_INV)
-        RoiEdges = cv2.Canny(RoiThreshold, 50, 150)
-        RoiContours, _ = cv2.findContours(RoiEdges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        RoiDetected = False
-        
-        if RoiContours:
-            RoiC = max(RoiContours, key=cv2.contourArea)
-            RoiMoments = cv2.moments(RoiC)
-            if RoiMoments["m00"] != 0:
-                RoiCx = int(RoiMoments["m10"] / RoiMoments["m00"])
-                RoiCy = int(RoiMoments["m01"] / RoiMoments["m00"])
-                if draw == True:
-                    # Gambar kontur dan centroid pada frame asli
-                    cv2.drawContours(CameraImage, [RoiC], -1, (0, 255, 0), 2)
-                    cv2.circle(CameraImage, (RoiCx, RoiCy), 7, (255, 0, 0), -1)
-                RoiDetected = True
-            else:
-                RoiCx, RoiCy = 0, 0
-            
-            #print(RoiDetected, Row, RoiCx, RoiCy)
-            return CameraImage, RoiDetected, RoiCx, RoiCy
-    
-
-    def GetErrorFollow(self, CameraImage, RoiCxFollow, SensorFollowWidth):
-        cv2.line(CameraImage, (self.CameraWidth // 2, 0), (self.CameraWidth // 2, self.CameraHeight), (0, 0, 255), 1)
-        #ErrorFollow = (RoiCxFollow - (55 // 2))
-        VerticalStart = int(self.CameraWidth * ((1-SensorFollowWidth)/2))
-        VerticalEnd = int(self.CameraWidth - self.CameraWidth * ((1-SensorFollowWidth)/2))
-        ErrorFollow = (RoiCxFollow - 56)
+    def GetErrorFollow(self, CameraImage, RoiCxFollow, SensorFollowWidth, drawLine=False):
+        if drawLine:
+            cv2.line(CameraImage, (self.CameraWidth // 2, 0), (self.CameraWidth // 2, self.CameraHeight), (0, 0, 255), 1)
+        ErrorFollow = (RoiCxFollow - (self.CameraWidth * SensorFollowWidth) // 2)
         return ErrorFollow
 
     def GetAngleSpeed(self, CameraImage, 
@@ -113,63 +81,37 @@ class LineFollower:
                       RoiCxSpeed, RoiCxFollow, 
                       SensorSpeedRow, SensorFollowRow, 
                       RoiCySpeed, RoiCyFollow, 
-                      draw=False):
-        AngleSpeed = 45
-        VerticalStartFollow = int(self.CameraWidth * ((1-SensorSpeedWidth)/2))
-        VerticalStartSpeed = int(self.CameraWidth * ((1-SensorFollowWidth)/2))
-        HorizontalStartFollow = self.RowHeight * SensorFollowRow
-        HorizontalStartSpeed = self.RowHeight * SensorSpeedRow
+                      drawDot=False, drawLine=False):
+        AngleSpeed = 45        
         if RoiDetectedSpeed and RoiDetectedFollow:
-            CentroidFollow = (VerticalStartFollow + RoiCxFollow, HorizontalStartFollow + RoiCySpeed)
-            CentroidSpeed = (VerticalStartSpeed + RoiCxSpeed, HorizontalStartSpeed + RoiCyFollow)
-            if draw:
-                cv2.line(CameraImage, CentroidFollow, CentroidSpeed, (255, 255, 0), 2)
+            FirstPointCx = int((RoiCxFollow - (self.CameraWidth * SensorFollowWidth) // 2) + (self.CameraWidth / 2))
+            FirstPointCy = int(self.RowHeight * SensorSpeedRow + RoiCySpeed)
+            
+            SecondPointCx = int((RoiCxFollow - (self.CameraWidth * SensorFollowWidth) // 2) + (self.CameraWidth / 2))
+            SecondPointCy = int(self.RowHeight * SensorFollowRow + RoiCyFollow)
+            
+            ThirdPointCx = int((RoiCxSpeed - (self.CameraWidth * SensorSpeedWidth) // 2) + (self.CameraWidth / 2))
+            ThirdPointCy = int(self.RowHeight * SensorSpeedRow + RoiCySpeed)
+            
+            PointA = np.array([FirstPointCx, FirstPointCy])
+            PointB = np.array([SecondPointCx, SecondPointCy])
+            PointC = np.array([ThirdPointCx, ThirdPointCy])
+            
+            PointBA = PointA - PointB
+            PointBC = PointC - PointB
+        
+            CosineAngle = np.dot(PointBA, PointBC) / (np.linalg.norm(PointBA) * np.linalg.norm(PointBC))
+            AngleRadian = np.arccos(CosineAngle)
+            AngleSpeed = np.degrees(AngleRadian)
+        
+            if drawDot:
+                cv2.circle(CameraImage, PointA, 3, (0, 0, 255), -1)
+                cv2.circle(CameraImage, PointB, 3, (0, 0, 255), -1)
+                cv2.circle(CameraImage, PointC, 3, (0, 0, 255), -1)
+            if drawLine:
+                cv2.line(CameraImage, PointA, PointB, (0, 255, 0), 2)
+                cv2.line(CameraImage, PointB, PointC, (0, 255, 0), 2)
 
-            DeltaX = CentroidSpeed[0] - CentroidFollow[0]
-            DeltaY = CentroidSpeed[1] - CentroidFollow[1]
-            AngleSpeed = math.degrees(math.atan2(DeltaY, DeltaX))
-
-            if AngleSpeed < -90:
-                AngleSpeed += 90
-            elif AngleSpeed > -90:
-                AngleSpeed -= -90
-            AngleSpeed = abs(AngleSpeed)
-            
-            if AngleSpeed < 10 and AngleSpeed > -10:
-                AngleSpeed = 0
-            else:
-                AngleSpeed = AngleSpeed
-        return AngleSpeed
-
-    def GetAngleSpeed2(self, CameraImage, RoiDetectedSpeed, RoiDetectedFollow, RoiCxSpeed, RoiCxFollow, RoiCySpeed, RoiCyFollow, draw=False):
-        AngleSpeed = 60
-        if RoiDetectedSpeed and RoiDetectedFollow:
-            '''FirstPoint = (RoiCxSpeed, RoiCySpeed)
-            SecondPoint = (RoiCxFollow, RoiCyFollow)
-            ThirdPoint = (RoiCxFollow, 0)
-            
-            FirstPoint = np.array(FirstPoint)
-            SecondPoint = np.array(SecondPoint)
-            ThirdPoint = np.array(ThirdPoint)
-            
-            FirstVector = FirstPoint - SecondPoint
-            SecondVector = ThirdPoint - SecondPoint
-            
-            DotProduct = np.dot(FirstVector, SecondVector)
-            FirstMagnitude = np.linalg.norm(FirstVector)
-            SecondMagnitude = np.linalg.norm(SecondVector)
-            
-            CosAngle = DotProduct / (FirstMagnitude * SecondMagnitude)
-            
-            AngleSpeed = np.arccos(CosAngle)
-            AngleSpeed = np.degrees(AngleSpeed)'''
-            
-            if draw:
-                cv2.circle(CameraImage, (RoiCxSpeed, RoiCySpeed), 5, (0, 255, 0), -1)
-                cv2.circle(CameraImage, (RoiCxFollow, RoiCyFollow), 5, (0, 255, 0), -1)
-                cv2.circle(CameraImage, (RoiCxFollow, 10), 5, (0, 255, 0), -1)
-                #cv2.line(CameraImage, FirstPoint, SecondPoint, (0, 255, 255), 2)
-                
         return AngleSpeed
 
     def ShowCamera(self, CameraImage):
@@ -207,8 +149,6 @@ class LineFollower:
 
         self.LeftMotor.setVelocity(LeftSpeed)
         self.RightMotor.setVelocity(RightSpeed)
-        
-        #print(f"Base Speed: {BaseSpeed:+.2f}, Steering: {SteeringFollow:+.2f}, Left Speed: {LeftSpeed:+.2f}, Right Speed: {RightSpeed:+.2f}")
     
     def cleanup(self):
         cv2.destroyAllWindows()
@@ -217,25 +157,25 @@ class LineFollower:
     def run(self):
         SensorSpeedRow = 2
         SensorSpeedWidth = 0.8
-        SensorFollowRow = 9
+        SensorFollowRow = 10
         SensorFollowWidth = 0.3
         while self.Robot.step(self.TimeStep) != -1:
             CameraImage = self.ReadCamera()
-            CameraImage, RoiDetectedSpeed, RoiCxSpeed, RoiCySpeed = self.GetReference(CameraImage, SensorSpeedRow, SensorSpeedWidth, draw=True)
-            CameraImage, RoiDetectedFollow, RoiCxFollow, RoiCyFollow = self.GetReference(CameraImage, SensorFollowRow, SensorFollowWidth, draw=True)
+            CameraImage, RoiDetectedSpeed, RoiCxSpeed, RoiCySpeed = self.GetReference(CameraImage, SensorSpeedRow, SensorSpeedWidth, drawDot=True, drawBox=True)
+            CameraImage, RoiDetectedFollow, RoiCxFollow, RoiCyFollow = self.GetReference(CameraImage, SensorFollowRow, SensorFollowWidth, drawDot=True, drawBox=True)
 
             if RoiDetectedFollow: 
-                ErrorFollow = self.GetErrorFollow(CameraImage, RoiCxFollow, SensorFollowWidth)
-                print(RoiDetectedFollow, RoiCxFollow, RoiCyFollow, ErrorFollow)
-            #AngleSpeed = self.GetAngleSpeed(CameraImage, RoiDetectedSpeed, RoiDetectedFollow, SensorSpeedWidth, SensorFollowWidth, RoiCxSpeed, RoiCxFollow, SensorSpeedRow, SensorFollowRow, RoiCySpeed, RoiCyFollow, draw=True)
+                ErrorFollow = self.GetErrorFollow(CameraImage, RoiCxFollow, SensorFollowWidth, drawLine=True)
+            AngleSpeed = self.GetAngleSpeed(CameraImage, RoiDetectedSpeed, RoiDetectedFollow, SensorSpeedWidth, SensorFollowWidth, RoiCxSpeed, RoiCxFollow, SensorSpeedRow, SensorFollowRow, RoiCySpeed, RoiCyFollow, drawDot=True, drawLine=True)
 
             #BaseSpeed = self.CalculateBaseSpeed(AngleSpeed)
             BaseSpeed = 2.0
             SteeringFollow = self.CalculateSteeringFollow(ErrorFollow)
 
-            #print(AngleSpeed, ErrorFollow)#, BaseSpeed, SteeringFollow)
-            
+            #print(AngleSpeed, ErrorFollow BaseSpeed, SteeringFollow)
             self.MotorAction(BaseSpeed, SteeringFollow)
+            print(f"Base Speed: {BaseSpeed:+.2f}, Steering: {SteeringFollow:+.2f}, Left Speed: {BaseSpeed+SteeringFollow:+.2f}, Right Speed: {BaseSpeed-SteeringFollow:+.2f}")
+            
             self.ShowCamera(CameraImage)
 
 
